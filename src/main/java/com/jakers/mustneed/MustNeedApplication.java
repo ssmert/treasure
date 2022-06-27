bfd96c5efd7f43b12b09653cec82422c80dbcec6
ssmert <ssmert503048@gmail.com>
2022-06-27 14:55:34 +0900

    NEW: init commit

---
+++ src/main/java/com/jakers/mustneed/MustNeedApplication.java
@@ -0,0 +1,13 @@
+package com.jakers.mustneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustNeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MustNeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakers/mustneed/MustNeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakers.mustneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustNeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
