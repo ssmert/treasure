216d63d187a9e258a6851681f07ba202f1be3529
ssmert <ssmert503048@gmail.com>
2022-06-28 16:23:14 +0900

    NEW: add SwaggerConfig

---
+++ src/main/java/com/jakers/mustneed/core/config/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package com.jakers.mustneed.core.config;
+
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger 설정
+ * <a href="http://localhost:8080/swagger-ui/">swagger</a>
+ */
+@EnableSwagger2
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+            .apiInfo(apiInfo())
+            .pathMapping("/")
+            .securityContexts(Collections.singletonList(securityContext()))
+            .securitySchemes(Collections.singletonList(apiKey()))
+            .useDefaultResponseMessages(false)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.jakers.mustneed"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Bearer", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Must Need")
+            .description("We Need This Project")
+            .license("Jakers. All rights reserved")
+            .version("1.0.0")
+            .build();
+    }
+}
