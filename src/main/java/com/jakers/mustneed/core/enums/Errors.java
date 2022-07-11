0841ce22733ad96476862140732b591211f7f8f6
ssmert <ssmert503048@gmail.com>
2022-07-11 11:52:17 +0900

    IMP : 에러 enum 추가

--- src/main/java/com/jakers/mustneed/core/enums/Errors.java
+++ src/main/java/com/jakers/mustneed/core/enums/Errors.java
@@ -11,6 +11,11 @@ import lombok.RequiredArgsConstructor;
 public enum Errors {
     UNAUTHORIZED(1000, "인증되지 않은 사용자입니다."),
     BAD_REQUEST(1001, "잘못된 요청입니다."),
+
+    NOT_FOUND_USER(2001, "존재하지 않는 사용자입니다."),
+
+    DUPLICATED_USER(3001, "동일한 사용자가 존재합니다."),
+
     INTERNAL_SERVER_ERROR(9999, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다.");
 
     private final int code;
